af41009ff917d7db788b6542d4136b1694e65b70
Flor <florenciar504@gmail.com>
2022-09-27 22:45:22 -0300

    Agrego componente Card (wip)

--- src/components/ContentRowMovies.jsx
+++ src/components/ContentRowMovies.jsx
@@ -1,25 +1,14 @@
+import Cart from "./Cart";
+
 
 function ContentRowMovies(){
+  const cartValues = [{titulo :'Movies in data base', color: 'blue', cifra: 21, icono: "fas fa-film fa-2x text-gray-300" },
+                     {titulo :'Total awards', color: '#1cc88a', cifra: 79, icono: "fas fa-award fa-2x text-gray-300" },
+                     {titulo :'Actors quantity', color: '#f6c23e', cifra: 49, icono: "fas fa-user fa-2x text-gray-300" }]
     return(
         <div class="row">
-        <div class="col-md-4 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                    Movies in Data Base
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-film fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+       
+        <Cart cartValues = {cartValues}/>
         <div class="col-md-4 mb-4">
           <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
